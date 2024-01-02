9f779b3656288f9aedb27840b8c87a78656552bc
Petrus <petrusheya@gmail.com>
2024-01-02 17:03:29 +0200

    Image upload displayed on canvas as background plus API POST feature

--- index.js
+++ index.js
@@ -7,7 +7,6 @@ const img = new Image()
 
 async function Base64FileReader(selector){
     //This function returns a Base64 encoding of a file. Give the target file input query selector as a parameter
-
     return new Promise((resolve,reject)=>{
 
         const fileInput = document.querySelector(selector)
@@ -26,7 +25,7 @@ async function Base64FileReader(selector){
             }
         }
 
-        fileReader.readAsDataURL(image)
+         fileReader.readAsDataURL(image)
 
     })
     
@@ -40,20 +39,50 @@ async function drawImageOnCanvas(input) {
 
     try {
 
-        const base64Data = await Base64FileReader(input)
-        img.onload = function(){
-            ctx.clearRect(0, 0, canvas.width, canvas.height);
-            ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
-        }
+        const img = new Image()
+        const base64Url = await Base64FileReader(input)
+        
+        img.onload = function() {
+            // Now the image is loaded, and you can access its dimensions
+            console.log(img.width);
+            console.log(img.height);
 
-        img.src = base64Data
-        console.log(base64Data)
+            // Set the canvas size and background properties
+            canvas.style.width = img.width + 'px';
+            canvas.style.height = img.height + 'px';
+            canvas.style.backgroundImage = `url(${img.src})`;
+            canvas.style.backgroundSize = 'cover';
+            canvas.style.backgroundPosition = 'center';
+        };
+
+        img.src = base64Url;
+
     }
     catch (error) {
         console.error('There was an error:' + error);
     }
 }
 
+
+
+async function imageToText(){
+    const fileInput = document.querySelector('input')
+
+    const imageFile = fileInput.files[0]
+
+    const formData = new FormData();
+
+    formData.append('image', imageFile);
+
+    try{
+        result = await fetch('https://api.api-ninjas.com/v1/imagetotext',{ method: 'POST', headers: {'X-Api-Key': 'im6UqOHTsLg5q/kQc5vy1Q==8KE0dN3KlXLZLUFc'}, body: formData})
+        console.log("YOYO")
+        const jsonResult = await result.json()
+    }
+    catch(error){
+        console.error(`Happy debuging✌: ${error.error}`)
+    }
+}
 
 
 
